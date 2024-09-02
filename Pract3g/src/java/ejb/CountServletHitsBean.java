d1879601036c646493387aab5814ebf40bd7c50e
rishiriii <rishiriii.10@gmail.com>
2024-09-02 08:02:17 +0530

    Add files via upload

---
+++ Pract3g/src/java/ejb/CountServletHitsBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import javax.ejb.Singleton;
+
+/**
+ *
+ * @author Admin
+ */
+@Singleton
+public class CountServletHitsBean {
+
+  private int hitCount;
+  
+  public synchronized int incrementAndGetHitCount(){
+      return hitCount++;
+  }
+}
